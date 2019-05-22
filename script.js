4dfb40a2d1b2543ada22e150729e39077a250b9a
VladimirTi <tytmes@mail.ru>
2019-05-22 23:45:42 +0300

    correction

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-document.querySelectorAll('li').forEach((item) => {
-  const span = document.createElement('span');
-  item.insertBefore(span, item.firstChild);
-  span.appendChild(span.nextSibling);
-});
-
 document.querySelector('.tree').addEventListener('click', (event) => {
-  if (event.target.tagName != 'SPAN') {
-    return;
-  }
-  const container = event.target.parentNode.getElementsByTagName('ul')[0];
-  if (!container) {
-    return;
-  }
+  if (event.target.tagName != 'SPAN') return;
+
+  const container = event.target.parentNode.querySelectorAll('ul')[0];
+  if (!container) return;
+
   container.hidden = !container.hidden;
 });
